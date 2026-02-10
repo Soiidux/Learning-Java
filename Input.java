e48ecc57b6e9a2d66f34195738a5fb9c683d69ed
Yash Nagpal <soiidux040@gmail.com>
2026-02-10 23:54:36 +0530

    First commit

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter integer input: ");
+        int rno = input.nextInt();
+        System.out.println("Your roll number is: " + rno);
+
+
+    }
+}
---
+++ Primitives.java
@@ -0,0 +1,18 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int roll_no = 37;
+        byte b = 100;
+        short s = 2000;
+        long largeInteger = 345678987656789L;
+
+        //String is not primitive
+        String name = "Yash";
+
+        char letter = 'r';
+
+        float marks = 98.67f;
+        double largeDecimalNumbers = 2348462312.235678;
+
+        boolean check = false;
+    }
+}
