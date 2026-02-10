da58de26d85389ed6b9a9e4eec68a414c95013f8
Yash Nagpal <soiidux040@gmail.com>
2026-02-11 00:17:10 +0530

    Inputs in java

--- Input.java
+++ Input.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 public class Input{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter integer input: ");
+        System.out.print("Enter integer input: ");
         int rno = input.nextInt();
         System.out.println("Your roll number is: " + rno);
+
+        input.nextLine(); //Nextline to clear \n buffer
+
+        System.out.print("Enter a string: ");
+        String name = input.nextLine();
+        System.out.println("Your name is: "+name);
+
+        System.out.print("Enter a floating point");
+        float marks = input.nextFloat();
+        System.out.println("Your marks are : "+marks);
 
 
     }
